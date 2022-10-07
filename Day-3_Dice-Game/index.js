972f24b3fa4e249a091b4a27d6711e0d4c6729cb
Your Name <you@example.com>
2022-10-07 20:16:46 +0530

    commit

--- Day-3_Dice-Game/index.js
+++ Day-3_Dice-Game/index.js
@@ -17,6 +17,23 @@ var randomImageSource2 = "images/dice" + randomNum2 + ".png";
 var image2 = document.querySelectorAll("img")[1];
 image2.setAttribute("src", randomImageSource2);
 
+// Player 3 setup
+var randomNum3 = Math.floor(Math.random() * 6) + 1;
+//map images to randomnumber to files in images folder
+var randomImageSource3 = "images/dice" + randomNum3 + ".png";
+
+// img[2] - 3rd image update as per random number
+var image3 = document.querySelectorAll("img")[2];
+image3.setAttribute("src", randomImageSource3);
+
+// Player 4 setup
+var randomNum4 = Math.floor(Math.random() * 6) + 1;
+//map images to randomnumber to files in images folder
+var randomImageSource4 = "images/dice" + randomNum4 + ".png";
+
+// img[3] - 4th image update as per random number
+var image4 = document.querySelectorAll("img")[3];
+image4.setAttribute("src", randomImageSource4);
 
 // Main Logic of Dice
 if(randomNum1 > randomNum2)
@@ -26,6 +43,14 @@ document.querySelector("h1").innerHTML = "Player 1 Wins !"
 else if (randomNum2 > randomNum1 )
 {
   document.querySelector("h1").innerHTML = "Player 2 Wins !"
+}
+else if (randomNum3 > randomNum2 )
+{
+  document.querySelector("h1").innerHTML = "Player 3 Wins !"
+}
+else if (randomNum4 > randomNum3 )
+{
+  document.querySelector("h1").innerHTML = "Player 4 Wins !"
 }
 else
 {
